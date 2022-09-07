0898a3424b28acd283a56f5b8311191f400eb5b1
IddrisuAminu <iddrisuaminuict@gmail.com>
2022-09-07 14:40:09 +0000

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDb = require("./config/connectDb")
+const userRoute= require("./userRoute/userRoutes")
 
+const morgan= require("morgan")
 dotenv.config();
 const app = express();
+connectDb();
+// middleware
+app.use(express.json());
+app.use("/user", userRoute);
+
+app.use(morgan("dev"))
+
 
 app.get("/", (req, res) => {
   res.send("<>Welcome to Lassia Tuoluu SHS>");
